ef38902b773f104ca0d69d7b3d6d4e76372dda52
Mohit Rawat <mohitrawat48@gmail.com>
2022-01-26 09:38:10 -0500

    set webapp as war name

--- src/main/java/com/demo/mnotesapp/mnotesappApplication.java
+++ src/main/java/com/demo/mnotesapp/mnotesappApplication.java
@@ -1,13 +1,22 @@
 package com.demo.mnotesapp;
 
+import javafx.application.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class mnotesappApplication {
+public class mnotesappApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(mnotesappApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(Application.class);
 	}
 
 }
